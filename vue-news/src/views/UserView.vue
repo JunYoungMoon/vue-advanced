08e800cc7813e810c2db8bc8adee534599af76a2
JunYoungMoon <ko5642027@naver.com>
2021-11-23 20:34:37 +0900

    UserView setting

--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-      Item
+    <p>id : {{ fetchedUser.id }}</p>
+    <p>karma : {{ fetchedUser.karma }}</p>
+    <p>joined : {{ fetchedUser.created }}</p>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 
 export default {
+  created() {
+    const userId = this.$route.params.id;
+    this.$store.dispatch('FETCH_USER', userId);
+  },
+  computed: {
+    ...mapGetters(['fetchedUser']),
+  },
 
 }
 </script>
