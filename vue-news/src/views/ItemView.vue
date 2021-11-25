296696899b2b9ef0040ed004a8f46746531d5d60
JunYoung <ko5642027@naver.com>
2021-11-25 09:41:33 +0900

    ASK 상세 내용 구현

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,15 +1,62 @@
 <template>
     <div>
-        News
+        <section>
+            <!-- 질문 상세 정보 -->
+            <div class="user-container">
+                <div>
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="user-description">
+                    <router-link :to="`/user/${fetchedItem.user}`">
+                        {{ fetchedItem.user}}
+                    </router-link>      
+                    <div class="time">
+                    {{ fetchedItem.time_ago}}
+                    </div>              
+                </div>                
+            </div>    
+            <h2>{{ fetchedItem.title}}</h2> 
+        </section>
+        <section>
+            <!-- 질문 댓글 -->
+            <div v-html="fetchedItem.content" class="content"></div>
+        </section>
     </div>
 </template>
 
 <script>
-export default {
+import { mapGetters } from 'vuex';
 
+export default {
+    computed:{
+        ...mapGetters(['fetchedItem']),
+    },
+    created(){
+        const itemId = this.$route.params.id;            
+		this.$store.dispatch('FETCH_ITEM',itemId)
+			.then(() => console.log('success'))
+			.catch(() => console.log('fail'));
+	}
 }
 </script>
 
-<style>
-
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
+.content {
+  border: ridge;
+  padding: 0.5rem;
+}
 </style>
